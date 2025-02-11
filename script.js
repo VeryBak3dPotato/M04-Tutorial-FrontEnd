ec36e6a6dffaa1c7482dea1ae788b9202c62e524
Connor Bailey <cebailey92@gmail.com>
2025-02-10 20:31:23 -0500

    correction 1

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Event listener
 addEventListener('DOMContentLoaded', async function(){
-    const response = await fetch("http://https://colossal-supreme-appeal.glitch.me/api/songs");
+    const response = await fetch("http://https://colossal-supreme-appeal.glitch.me");
     const songs = await response.json();
 
     let html = '';
